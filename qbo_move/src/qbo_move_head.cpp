6e9237739955be7b63755aa732a526bfee18e83e
Manon Cortial <mc@generationrobots.com>
2014-10-30 18:23:56 +0100

    comented qbo_move_head

--- qbo_move/src/qbo_move_head.cpp
+++ qbo_move/src/qbo_move_head.cpp
@@ -9,13 +9,15 @@
 //pointer to nodeHandle so the callback can access to it too
 ros::NodeHandle* n;
 
+//publisher to move the head (to /cmd_joints)
 ros::Publisher* move_head;
+
 //variables for head PIDs
 ros::Time timePreviousTilt;
-
 int zeroTilt = 0;
 double tiltRadPerTicks = 0;
 
+  //tilt values to compute PID
 double currentTilt = 0;
 double previousTilt = 0;
 double iTilt = 0;
@@ -23,6 +25,7 @@ double iTilt = 0;
 int zeroPan = 0;
 double panRadPerTicks = 0;
 
+  //pan values to compute PID
 double currentPan = 0;
 double previousPan = 0;
 double iPan = 0;
@@ -33,10 +36,10 @@ void initValues()
 {
     //get the zero values (in ticks)
     n->getParam("/qbo_arduqbo/dynamixelservo/head_tilt_joint/neutral", zeroTilt);
-    ros::param::get("/qbo_arduqbo/dynamixelservo/head_pan_joint/neutral", zeroPan);
+    n->getParam("/qbo_arduqbo/dynamixelservo/head_pan_joint/neutral", zeroPan);
 
 //get the range (in degrees) and the range (in ticks)
-//calculate the convertion
+//compute the number of ticks per radians
     double range;
     n->getParam("/qbo_arduqbo/dynamixelservo/head_tilt_joint/range", range);
     int ticks;
@@ -57,10 +60,11 @@ void initValues()
     ros::param::set("/qbo_head_tracker/tilt/kd", 0.07);
     ros::param::set("/qbo_head_tracker/tilt/ki", 0.005);
 
-
+//last time we got a tilt value (used to compute differential factor of PID)
     timePreviousTilt = ros::Time::now();
 }
 
+//empty service to tell other nodes we are ready
 bool isReadyService(std_srvs::Empty::Request  &req,
                     std_srvs::Empty::Response &res)
 {
@@ -86,27 +90,29 @@ float controlPID(float x, float ix, float dx, float Kp, float Ki, float Kd)
     return Kp*x+Ki*ix+Kd*dx;
 }
 
-
+//move the head to the absolute position (pan, tilt)
 void moveHeadAbs(double pan, double tilt)
 {
+//ROS_INFO("Moving head at: [x: %lf, y: %lf]", pan, tilt);
+
+//create JointState message
     sensor_msgs::JointState mvmt_msg;
+
     std::vector<std::string> names;
     names.push_back("head_pan_joint");
     names.push_back("head_tilt_joint");
-
     mvmt_msg.name = names;
+
     std::vector<double> position;
     position.push_back(pan);
     position.push_back(tilt);
-
-//ROS_INFO("Moving head at: [x: %lf, y: %lf]", pan, tilt);
-
     mvmt_msg.position = position;
 
-//   ROS_INFO("move head to %lf, %lf", pan, tilt);
+//publish message
     move_head->publish(mvmt_msg);
 }
 
+//move the head at (pan, tilt) from its current position
 void moveHeadRel(double pan, double tilt)
 {
     /****** PID calculation for tilt ***********/
@@ -140,9 +146,9 @@ void moveHeadRel(double pan, double tilt)
 
     double  panPID = controlPID(pan, iPan, dPan, kp, kd, ki);
 
-//move head
+//compute absolute position and use moveHeadAbs
     moveHeadAbs(currentPan + panPID, currentTilt + tiltPID);
-ROS_INFO("Moving head from: [x: %lf, y: %lf]", panPID, tiltPID);
+//ROS_INFO("Moving head from: [x: %lf, y: %lf]", panPID, tiltPID);
 
 //update
     timePreviousTilt = now;
@@ -150,34 +156,34 @@ ROS_INFO("Moving head from: [x: %lf, y: %lf]", panPID, tiltPID);
     previousPan = currentPan;
 }
 
-
+//move eyelids to (left, right) absolute position
 void moveEyelidsAbs(double left, double right)
 {
+//create JointState message
     sensor_msgs::JointState mvmt_msg;
+
     std::vector<std::string> names;
     names.push_back("left_eyelid_joint");
     names.push_back("right_eyelid_joint");
-
     mvmt_msg.name = names;
+
     std::vector<double> position;
     position.push_back(left);
     position.push_back(right);
-
     mvmt_msg.position = position;
 
-//   same topic as the head
+//  send message (same topic as the head)
     move_head->publish(mvmt_msg);
 }
 
 /**********
-HR_move_head message :
-pan
-tilt
-isAbsolute
+geometry_msgs::Point message :
+(x, y) -> (pan, tilt)
+z>0 -> absolute move, relative move otherwise
 ***********/
 void qboMoveHeadCallback(const geometry_msgs::Point::ConstPtr& msg)
 {
-    // ROS_INFO("movement order: [pan: %lf, tilt: %lf]", msg->pan, msg->tilt);
+    // ROS_INFO("movement order: [pan: %lf, tilt: %lf]", msg->x, msg->y);
 
     if(msg->z > 0) //absolute move
     {
@@ -187,40 +193,39 @@ void qboMoveHeadCallback(const geometry_msgs::Point::ConstPtr& msg)
     {
         moveHeadRel( msg->x,  msg->y);
     }
-
 }
 
 
 /**********
-HR_move_eyelids message :
-left
-right
+geometry_msgs::Point message :
+(x, y) -> (left, right)
+z unused
 ***********/
 void qboMoveEyelidsCallback(const geometry_msgs::Point::ConstPtr& msg)
 {
-    //  ROS_INFO("eyelid order: [left: %lf, right: %lf]", msg->left, msg->right);
+    //  ROS_INFO("eyelid order: [left: %lf, right: %lf]", msg->x, msg->y);
     moveEyelidsAbs(msg->x, msg->y);
 }
 
 
 /**********
-HR_tracker message :
-x = position in width in the image. In range [-1 (left), +1 (right)]
-y = position in height in the image. In range [-1 (up), +1 (down)]
-distance : distance to object (unused if 0 or less)
+geometry_msgs::PointStamped message :
+stamp : unused at the moment
+point.x = position in width in the image. In range [-1 (left), +1 (right)]
+point.y = position in height in the image. In range [-1 (up), +1 (down)]
+point.z : distance to object (unused if 0 or less)
 ***********/
 void trackerCallback(const geometry_msgs::PointStamped::ConstPtr& msg)
 {
-    // ROS_INFO("tracker callback for %s: [x: %lf, y: %lf]", msg->object_name.c_str(), msg->x, msg->y);
+    // ROS_INFO("tracker callback: [x: %lf, y: %lf, z: %lf]", msg->point.x, msg->point.y, msg->point.z);
 if(msg->point.z > 0){
-ROS_INFO("Got image position: [x: %lf, y: %lf]", msg->point.x, msg->point.y);
 	double currentObjectPosX = msg->point.x;
-double currentObjectPosY = msg->point.y;
-double objectTargetX = 0.0;
-double objectTargetY = 0.0;
+	double currentObjectPosY = msg->point.y;
+	double objectTargetX = 0.0; //TODO : read target position from parameters or from another callback
+	double objectTargetY = 0.0;
 
-    moveHeadRel(-currentObjectPosX+objectTargetX , -objectTargetY + currentObjectPosY );
-}
+    moveHeadRel(objectTargetX-currentObjectPosX , currentObjectPosY-objectTargetY );
+	}
 
 }
 
@@ -228,37 +233,39 @@ double objectTargetY = 0.0;
 
 int main(int argc, char **argv)
 {
+//init node
     ros::init(argc, argv, "qbo_move_head");
     n = new ros::NodeHandle();
 
-ros::service::waitForService("qbo_arduqbo/test_service", -1);
+//wait for qbo_arduqbo node to be ready
+    ros::service::waitForService("qbo_arduqbo/test_service", -1);
 
+//initialize parameter values
     initValues();
+
 /****
 * Subscribers
 ****/
+//subscribe to joint states to get the current position (in tilt)
     ros::Subscriber tilt_sub = n->subscribe("qbo_arduqbo/head_tilt_joint/state", 10, qboTiltCallback);
     ros::Subscriber pan_sub  =  n->subscribe("qbo_arduqbo/head_pan_joint/state", 10, qboPanCallback);
 
+//subscribe to topics where someone asks the head to move
     ros::Subscriber move_head_sub  = n->subscribe("qbo_move_head", 10, qboMoveHeadCallback);
-
     ros::Subscriber move_eyelids_sub  = n->subscribe("qbo_move_eyelids", 10, qboMoveEyelidsCallback);
-
     ros::Subscriber tracker_sub  = n->subscribe("qbo_head_tracker", 10, trackerCallback);
 
 /****
 * Publisher
 ****/
+//publisher to /cmd_joint to give orders to the motors through the qbo_arduqbo node
     ros::Publisher temp = n->advertise<sensor_msgs::JointState>("/cmd_joints", 10);
     move_head = &(temp);
 
-   /*ros::Rate loop_rate(1);
-    loop_rate.sleep();
-    loop_rate.sleep();
-    moveHeadAbs(0., 0.);*/
     ROS_INFO("qbo_move_head node ready");
     ros::ServiceServer serviceReady = n->advertiseService("qbo_move_head/isReady", isReadyService);
 
+//run until stopped
     ros::spin();
     return 0;
 }
